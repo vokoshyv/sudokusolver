c4697268684cd6df9328dba45d922abcc5f452dd
vokoshyv <ronsuij@gmail.com>
2015-07-19 15:13:19 -0700

    initializes code for grabbing data from input fields to solve puzzle

---
+++ sudoku.js
@@ -0,0 +1,22 @@
+var solveThePuzzle = function(){
+  var work = [];
+  var addTimes = 9;
+
+  // creates empty matrix
+  while (addTimes--){
+    work.push([]);
+  }
+
+  // value of matrix to add starting from top left going to
+  // bottom right
+  var valueOfMatrix = 1;
+
+  for (var i = 0; i < 9; i++){
+    for (var j = 0; j < 9; j++){
+      work[i].push(Number(document.))
+    }
+  }
+
+  console.table(work);
+  debugger;
+}
