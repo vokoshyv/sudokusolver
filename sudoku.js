bced9976f0ac29c67354d60346dd79a787cd15f7
vokoshyv <ronsuij@gmail.com>
2015-07-19 16:27:08 -0700

    preparing html table to be added that has solved solution to sudoku puzzle

--- sudoku.js
+++ sudoku.js
@@ -10,13 +10,26 @@ var solveThePuzzle = function(){
   // value of matrix to add starting from top left going to
   // bottom right
   var valueOfMatrix = 1;
+  var currID;
 
   for (var i = 0; i < 9; i++){
     for (var j = 0; j < 9; j++){
-      work[i].push(Number(document.))
+      currID = "numberValue" + valueOfMatrix.toString();
+      work[i].push(Number(document.getElementById(currID).value))
+      valueOfMatrix++;
     }
   }
 
-  console.table(work);
+  var result = sudoku(work);
+
+  var solutionTable = document.createElement("TABLE");
+
+
   debugger;
+  document.getElementsByClassName("sudokuInputArea")[0].appendChild(solutionTable);
+
+  console.table(result);
+
+  debugger;
+
 }
